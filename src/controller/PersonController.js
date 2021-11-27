ada1b429c78e0fbf54589937ac12c90d467b16de
Oleg Gupalo <gupalooleg@gmail.com>
2021-11-27 18:43:59 +0300

    feat: add personController module

---
+++ src/controller/PersonController.js
@@ -0,0 +1,14 @@
+import { validate as uuidValidate } from 'uuid';
+import { PersonModel } from '../model/index.js';
+
+class PersonController {
+  get(req, res) {}
+
+  post(req, res) {}
+
+  put(req, res) {}
+
+  delete(req, res) {}
+}
+
+export { PersonController };
