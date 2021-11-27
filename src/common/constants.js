22f7d71fd3bff60916e150a773a36f0a085efb27
Oleg Gupalo <gupalooleg@gmail.com>
2021-11-27 18:28:16 +0300

    feat: add new messages

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,7 @@
 const MESSAGES = {
-  PERSON_NOT_FOUND: 'Person with identifier "{0}" not found.',
+  PERSON_NOT_FOUND: 'A person with identifier "{0}" not found.',
+  RESOURCE_NOT_FOUND: 'The resource at the specified URL "{0}" not found.',
+  HTTP_METHOD_NOT_SUPPORTED: 'The server does not support the HTTP "{0}" method.',
 };
 
 export { MESSAGES };
