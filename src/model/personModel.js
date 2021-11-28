8ab9f9e3ba7c346702ae8ef92b552ab3960eec31
Oleg Gupalo <gupalooleg@gmail.com>
2021-11-28 15:10:20 +0300

    fix: change personModel module

--- src/model/personModel.js
+++ src/model/personModel.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import { SimpleApiSchema } from '../db/index.js';
 import { PersonModelError } from '../error/index.js';
 import { MESSAGES, formatString } from '../common/index.js';
@@ -6,6 +5,7 @@ import { MESSAGES, formatString } from '../common/index.js';
 class PersonModel {
   constructor() {
     this.persons = SimpleApiSchema.Person;
+    this.personModelRequiredFields = ['name', 'age', 'hobbies'];
   }
 
   getAll() {
@@ -15,22 +15,20 @@ class PersonModel {
   getById(id) {
     const person = this.persons.find((value) => value.id === id);
     if (!person) {
-      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [id]));
+      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [id]), 404);
     }
 
     return person;
   }
 
   create(person) {
-    const newPerson = person;
-    newPerson.id = uuid();
-    this.persons.push(newPerson);
+    this.persons.push(person);
   }
 
   update(person) {
     const index = this.persons.findIndex((value) => value.id === person.id);
     if (index === -1) {
-      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [person.id]));
+      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [person.id]), 404);
     }
 
     this.persons[index] = person;
@@ -39,7 +37,7 @@ class PersonModel {
   delete(id) {
     const index = this.persons.findIndex((value) => value.id === id);
     if (index === -1) {
-      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [id]));
+      throw new PersonModelError(formatString(MESSAGES.PERSON_NOT_FOUND, [id]), 404);
     }
 
     this.persons.splice(index, 1);
